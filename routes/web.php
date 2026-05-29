2f21a5d14dc6a2497af6a6478f8e307d39521a14
GUGUN4569 <cahyaaris739@gmail.com>
2026-05-29 17:20:05 +0700

    fourth commit

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\NotaController;
 use App\Http\Controllers\FakturController;
+use App\Http\Controllers\DetailFakturController; 
+use App\Http\Controllers\DetailNotaController;
  
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
  
@@ -15,7 +17,7 @@ Route::resource('barang', BarangController::class);
 Route::resource('customer', CustomerController::class);
 Route::resource('supplier', SupplierController::class);
 Route::resource('pegawai', PegawaiController::class);
-Route::resource('nota', NotaController::class);
+Route::resource('nota', NotaController::class)->parameters(['nota' => 'nota']);
 Route::resource('faktur', FakturController::class);
 Route::resource('detail-faktur', DetailFakturController::class);
 Route::resource('detail-nota', DetailNotaController::class);
